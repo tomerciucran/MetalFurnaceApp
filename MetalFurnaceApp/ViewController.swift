51a7a6bd57d8f25481382be4d25d334c33dcb61d
Tomer Ciucran <tomer.ciucran@etherapists.de>
2018-01-03 10:39:24 +0100

    Initial Commit

---
+++ MetalFurnaceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalFurnaceApp
+//
+//  Created by Tomer Ciucran on 03.01.18.
+//  Copyright © 2018 Cihan Muradoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
